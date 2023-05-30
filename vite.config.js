3c1e657fe3f0d9477defc013d529159de7373be6
jotone <hereyouare1987@gmail.com>
2023-05-30 19:35:06 +0200

    File browser Controller

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
+import postcssNesting from 'postcss-nesting';
+import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({}),
+        postcssNesting
+      ],
+    },
+  },
+  plugins: [
+    laravel({
+      input: ['resources/css/file-browser.scss'],
+      refresh: true,
+    }),
+  ],
 });
